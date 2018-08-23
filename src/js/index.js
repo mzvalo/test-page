070d2a91e2fd42b5b68935266aede5ff70f76756
Marian Zvalo <marian.zvalo@webcreators.sk>
2018-08-23 16:09:59 +0200

    Pridavam index.php

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 function component() {
     let element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack Marian test'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
     return element;
 }
